63a4502753494b4310ac850e021da61bf72d6f24
Сергей Морозов <Tyuiu.MorozovMS@outlook.com>
2024-09-20 14:05:42 +0500

    Task4

---
+++ Tyuiu.MorozovSM.Sprint1.Task4.V20.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.MorozovSM.Sprint1.Task4.V20.Lib
+{
+    public class DataService : ISprint1Task4V20
+    {
+        public double Calculate(double x, double y)
+        {
+            double res = double.Round((1+x)/Math.Abs(x-Math.Sqrt(2+y)),3);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.MorozovSM.Sprint1.Task4.V20.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+using Tyuiu.MorozovSM.Sprint1.Task4.V20.Lib;
+
+namespace Tyuiu.MorozovSM.Sprint1.Task4.V20.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double y = 2;
+            double wait = 2;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
